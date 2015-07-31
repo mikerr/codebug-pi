76c4d6e1fb5dd13cce441f528da63475cc831c0c
root <root@raspberrypi.(none)>
2015-07-31 16:34:39 +0100

    pixel text - flashes random pixels on the codebug

---
+++ pixels.py
@@ -0,0 +1,9 @@
+import time, random
+import codebug_i2c_tether
+
+with codebug_i2c_tether.CodeBug() as codebug:
+	while True:
+		x = random.randint(0,4)
+		y = random.randint(0,4)
+		onoff = random.randint(0,1)
+		codebug.set_pixel(x, y, onoff)
